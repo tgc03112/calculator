7c93e11dfb8e79b1c9918625e953c5e49ce5adb2
tgc03112 <tgc03112@gmail.com>
2019-04-30 21:30:07 -0700

    Add calculator source and unit test

--- calculator/src/main/java/com/tgc03112/calculator/Calculator.java
+++ calculator/src/main/java/com/tgc03112/calculator/Calculator.java
@@ -1,11 +1,14 @@
 package com.tgc03112.calculator;
 import org.springframework.stereotype.Service;
+/** This is calcluator sevice class
+    tgc03112
+*/
 @Service
 public class Calculator {
-	int sum(int a, int b){
+	int sum(int a,int b){
 		return a+b;
 	}
-	int dif(int a, int b){
+	int dif(int a,int b){
 		return a-b;
 	}
 }
--- calculator/src/test/java/com/tgc03112/calculator/CalculatorTest.java
+++ calculator/src/test/java/com/tgc03112/calculator/CalculatorTest.java
@@ -1,14 +1,27 @@
 package com.tgc03112.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+
+/**
+  * Used for interace test
+  * @author tgc03112
+  */
 public class CalculatorTest {
 	private Calculator calculator = new Calculator();
 
 	@Test
+	/**
+	  * Used for interace test
+	  * @author tgc03112
+	  */
 	public void testSum(){
 		assertEquals(5, calculator.sum(2,3));
 	}
 	@Test
+	/**
+	  * Used for interace test
+	  * @author tgc03112
+	  */
 	public void testDif(){
 		assertEquals(-1, calculator.dif(2,3));
 	}
