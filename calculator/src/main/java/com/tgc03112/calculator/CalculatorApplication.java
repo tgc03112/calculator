8517d9d2e16d0009b38d749df2c05238dabda114
tgc03112 <tgc03112@gmail.com>
2019-05-01 07:15:36 -0700

    Add all

--- calculator/src/main/java/com/tgc03112/calculator/CalculatorApplication.java
+++ calculator/src/main/java/com/tgc03112/calculator/CalculatorApplication.java
@@ -3,6 +3,9 @@ package com.tgc03112.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** This is sample JAVADOC comments
+    Just a Sample
+*/
 @SpringBootApplication
 public class CalculatorApplication {
 
